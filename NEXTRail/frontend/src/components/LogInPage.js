cc666f6e4b8bc6282d7d7a4b9d1e3e3010214a9e
sohumsikdar <sohum20339@iiitd.ac.in>
2022-04-19 04:17:20 +0530

    added card to login

--- NEXTRail/frontend/src/components/LogInPage.js
+++ NEXTRail/frontend/src/components/LogInPage.js
@@ -1,88 +1,70 @@
 import React, { Component } from "react";
-import {Typography, AppBar, Card, CardAction, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button, Box, TextField} from "@material-ui/core";
+import { Typography, AppBar, Card, CardAction, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button, Box, TextField } from "@material-ui/core";
+
+const textbox = {
+	width: "450px",
+	height: "55px",
+	fontSize: "14px",
+	backgroundColor: "#FFFFFF"
+};
+
+const loginbut = {
+	backgroundColor: '#DC532D',
+	color: '#FFFFFF',
+	width: "225px",
+	height: "45px",
+	fontSize: "14px"
+};
+
+const signup = {
+	color: '#242038',
+	width: "112px",
+	height: "20px",
+	fontSize: "12px"
+};
+
 
 export default class LogInPage extends Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    return (
-      <div>
-        <Container
-          maxWidth="sm"
-          style={{
-            marginTop: "170px",
-          }}
-        >
-          <Typography
-            style={{ fontWeight: 650, color: "#242038" }}
-            variant="h1"
-            align="center"
-            position="relative"
-            gutterBottom
-          >
-            Log In
-          </Typography>
-          <div>
-            <Grid container spacing={2} align="center" direction="column">
-              <Grid item xs={16}>
-                <TextField
-                  style={{
-                    width: "450px",
-                    height: "55px",
-                    fontSize: "14px",
-                    backgroundColor: "#FFFFFF",
-                  }}
-                  id="outlined-basic"
-                  label="Username"
-                  variant="outlined"
-                />
-              </Grid>
-              <Grid item xs={16}>
-                <TextField
-                  style={{
-                    width: "450px",
-                    height: "55px",
-                    fontSize: "14px",
-                    backgroundColor: "#FFFFFF",
-                  }}
-                  id="outlined-basic"
-                  label="Password"
-                  variant="outlined"
-                />
-              </Grid>
-              <Grid item xs={16}>
-                <Button
-                  style={{
-                    backgroundColor: "#DC532D",
-                    color: "#FFFFFF",
-                    width: "225px",
-                    height: "45px",
-                    fontSize: "14px",
-                  }}
-                  variant="contained"
-                >
-                  Log In
-                </Button>
-              </Grid>
-              <Grid item style={{ marginTop: "-10px" }}>
-                <Button
-                  style={{
-                    color: "#242038",
-                    width: "112px",
-                    height: "20px",
-                    fontSize: "12px",
-                  }}
-                  variant="text"
-                >
-                  Sign Up
-                </Button>
-              </Grid>
-            </Grid>
-          </div>
-        </Container>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Container maxWidth="sm" style={{
+					marginTop: '130px'
+				}}>
+					<Card style={{ display: "block", height: "465px" }}>
+						<CardContent>
+							<div align="center">
+								<Avatar sx={{ m: 1 }} style={{ backgroundColor: "#388087", marginTop: "20px" }}>
+									<LockOutlinedIcon />
+								</Avatar>
+								<Typography style={{ fontWeight: 500, color: '#242038', marginTop: '1px' }} variant="h3" align="center" position="relative" gutterBottom>
+									Log In
+								</Typography>
+							</div>
+							<div style={{ marginTop: "50px" }}>
+								<Grid container spacing={2} align="center" direction="column">
+									<Grid item xs={16}>
+										<TextField style={textbox} id="outlined-basic" label="Username" variant="outlined" />
+									</Grid>
+									<Grid item xs={16}>
+										<TextField style={textbox} id="outlined-basic" label="Password" variant="outlined" />
+									</Grid>
+									<Grid item xs={16} style={{ marginTop: "30px" }}>
+										<Button style={loginbut} variant="contained">Log In</Button>
+									</Grid>
+									<Grid item style={{ marginTop: "-10px" }}>
+										<Button style={signup} variant="text">Sign Up</Button>
+									</Grid>
+								</Grid>
+							</div>
+						</CardContent>
+					</Card>
+				</Container>
+			</div>
+		);
+	}
 }
