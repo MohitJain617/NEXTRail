e8662bca3a9b15a5006a67509ce545f1d0cce17c
sohumsikdar <sohum20339@iiitd.ac.in>
2022-04-18 18:14:40 +0530

    PNR Screen

---
+++ react-website-test/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+const App = () => {
+    return (
+        <div>
+            <Typography variant="h1">Hello, World!</Typography>
+        </div>
+    );
+}
+
+export default App;
