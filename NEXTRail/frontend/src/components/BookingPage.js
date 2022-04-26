c85adeb9b7f034aff2696c735085dfae0cf3b63e
sohumsikdar <sohum20339@iiitd.ac.in>
2022-04-26 19:56:27 +0530

    idk what im doing

--- NEXTRail/frontend/src/components/BookingPage.js
+++ NEXTRail/frontend/src/components/BookingPage.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { Typography, AppBar, MenuItem, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button, Box, TextField } from "@material-ui/core";
+import TrainIcon from '@mui/icons-material/Train';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import TripCard from './TripCard.js'
 
-export default function BookingPage(props){
-    return(
+export default function BookingPage() {
+    return (
         <div>
-        { props.isAuth? <h1>This is Booking Page.</h1> : <h1>This is not a Booking Page.</h1>}
+            <Box maxWidth="sm" style={{
+                marginTop: '0px', background: '#C4C4C4', overflowY: 'hidden', width: '100%', height: "100px"
+            }}>
+                <Typography style={{ fontWeight: 550, marginTop: "20px" }} variant="h3" align="center" position="relative" gutterBottom>
+                    Upcoming Trips
+                </Typography>
+            </Box>
+            <div style={{ marginTop: "40px", marginLeft: "20%", marginRight: "20%" }}>
+                    {/* add cards here */}
+            <TripCard />
+
+            </div>
         </div>
     );
 }
