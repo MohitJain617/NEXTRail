5ebecad8ea76e13645e8ec94d0097af686c847f5
sohumsikdar <sohum20339@iiitd.ac.in>
2022-04-30 18:47:19 +0530

    Counting inversions

--- NEXTRail/frontend/src/components/AddressForm.js
+++ NEXTRail/frontend/src/components/AddressForm.js
@@ -4,37 +4,19 @@ import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
+import PassengerDetails from './PassengerDetails';
 
 export default function AddressForm() {
   return (
     <React.Fragment>
-      <Typography variant="h6" gutterBottom>
+      <Typography variant="h5" gutterBottom align='center' marginTop="-10px">
         Passenger Details
       </Typography>
+
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="firstName"
-            name="firstName"
-            label="First name"
-            fullWidth
-            autoComplete="given-name"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="lastName"
-            name="lastName"
-            label="Last name"
-            fullWidth
-            autoComplete="family-name"
-            variant="standard"
-          />
-        </Grid>
+        <PassengerDetails/>    
+        <PassengerDetails/>    
       </Grid>
-    </React.Fragment>
+    </React.Fragment >
   );
 }
---
+++ NEXTRail/frontend/src/components/PassengerDetails.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+
+export default function PassengerDetails() {
+    const gen = [
+        { label: "Male", code: 1 },
+        { label: "Female", code: 2 },
+        { label: "Others", code: 3 },
+    ];
+    const meal = [
+        { label: "Veg", code: 1 },
+        { label: "Non-veg", code: 2 },
+    ];
+    return (
+        <>
+            <Grid item xs={12} sm={12} marginBottom="-30px">
+                {/* add passenger number dynamically */}
+                <Typography variant="h6" gutterBottom align='left'>
+                    Passenger 1
+                </Typography>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <TextField
+                    required
+                    id="Name"
+                    name="Name"
+                    label="Name"
+                    fullWidth
+                    variant="standard"
+                />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <TextField
+                    required
+                    id="age"
+                    name="age"
+                    label="Age"
+                    fullWidth
+                    variant="standard"
+                />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <TextField
+                    select
+                    required
+                    id="Gender"
+                    name="Gender"
+                    label="Gender"
+                    fullWidth
+                    variant="standard"
+                >   
+                    {gen.map((option) => (
+                        <option key={option.value} value={option.value}>
+                            {option.label}
+                        </option>
+                    ))}
+                </TextField>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <TextField
+                    select
+                    required
+                    id="meal"
+                    name="meal"
+                    label="Meal Option"
+                    fullWidth
+                    variant="standard"
+                    style={{ backgroundColor: "#FFFFFF" }}
+                >
+                    {meal.map((option) => (
+                        <option key={option.value} value={option.value}>
+                            {option.label}
+                        </option>
+                    ))}
+                </TextField>
+            </Grid>
+
+        </>
+    );
+}
