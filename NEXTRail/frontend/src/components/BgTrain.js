28ceded76621b091b7d3758adc3bd1c411288d9f
sohumsikdar <sohum20339@iiitd.ac.in>
2022-04-30 17:57:30 +0530

    Changed train bg position

--- NEXTRail/frontend/src/components/BgTrain.js
+++ NEXTRail/frontend/src/components/BgTrain.js
@@ -2,17 +2,21 @@ import React from "react";
 
 import trainimg from "../static/img/train.svg";
 import bridgeimg from "../static/img/bridge.svg";
-import { Grid } from "@material-ui/core";
+import { Grid, Stack, Box} from "@material-ui/core";
 
 export default function BgTrain() {
   return (
-      <Grid container spacing={4}>
-          <Grid item xs={8}>
-          <img src={trainimg} alt="trainimg" style={{ position: "fixed" }} />
-          </Grid>
-          <Grid item xs={8}>
-          <img src={bridgeimg} alt="bridgeimg" style={{ width: "100%", position: "fixed" }} />
-          </Grid>
-      </Grid>
+    <> 
+      <Box>
+        <Box item marginTop="68px">
+          <img src={trainimg} alt="train" style={{ position: "fixed" }} />
+        </Box>
+      </Box>
+      <Box>
+        <Box item marginTop="200px">
+          <img src={bridgeimg} alt="bridgeimg" style={{ position: "fixed", width: "100%" }} />
+        </Box>
+      </Box>
+    </>
   );
 }
