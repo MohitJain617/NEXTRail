9f41cd36cf5e883be86bb828b87a0aadad080da9
Aadit Kant Jha <aadit20001@iiitd.ac.in>
2022-04-30 16:03:49 +0530

    Added payment form

--- NEXTRail/frontend/src/components/TrainBwStation.js
+++ NEXTRail/frontend/src/components/TrainBwStation.js
@@ -64,12 +64,21 @@ function TrainBwStation(props) {
 			label: "Second Seating",
 			code: "D",
 		},
-	];
+	  ];
+	const passes = [
+    { label: "1 Passenger", code: 1},
+    { label: "2 Passengers", code: 2},
+    { label: "3 Passengers", code: 3},
+    { label: "4 Passengers", code: 4},
+    { label: "5 Passengers", code: 5},
+    { label: "6 Passengers", code: 6},
+  ]; 
+  
+  const [result, setResult] = React.useState(false);
+  const [fadeIn, setFadeIn] = React.useState(0);
+  const [data, setData] = React.useState("");
+  const [moveUp, setMoveUp] = React.useState(0);
 
-	const [result, setResult] = React.useState(false);
-	const [fadeIn, setFadeIn] = React.useState(0);
-	const [data, setData] = React.useState("");
-	const [moveUp, setMoveUp] = React.useState(0);
 
 	const [value, setValue] = React.useState(new Date());
 	const [stnList, setStnList] = React.useState([]);
@@ -96,8 +105,8 @@ function TrainBwStation(props) {
 			rqstParam.dest = val.st_code;
 		}
 	}
-
-	function searchTrain() {
+  
+  function searchTrain() {
 		rqstParam.doj = value.toLocaleDateString("en-CA");
 		if (rqstParam.src === null || rqstParam.dest === null) {
 			props.sendAlert("Source or Destination missing!", WARNING);
@@ -127,140 +136,161 @@ function TrainBwStation(props) {
 		}
 	}
 
-	function getStns() {
-		fetch("data/stations")
-			.then((response) => response.json())
-			.then((data) => {
-				setStnList(data);
-			});
-	}
-	useEffect(() => {
-		getStns();
-	}, []);
 
-	return (
-		<>
-			<div>
-				<Box className="box" moveDown={moveUp} maxWidth="sm"></Box>
-			</div>
-			<div className="search_field" moveUp={moveUp}>
-				<Container
-					maxWidth="sm"
-					style={{
-						marginTop: "180px",
-					}}
-				>
-					<Typography
-						style={{ color: "#242038", fontWeight: 550 }}
-						variant="h3"
-						color="common.white"
-						justifyContent="center"
-						align="center"
-						position="relative"
-						gutterBottom
-					>
-						Your Journey starts here
-					</Typography>
-				</Container>
-				<Container align="center">
-					<div>
-						<Grid container spacing={0} align="center" justifyContent="center">
-							<Grid item xs={0}>
-								<StnAutoComplete
-									label="From"
-									stnList={stnList}
-									handler={getSrc}
-								/>
-							</Grid>
-							<Grid item xs={0}>
-								<StnAutoComplete label="To" stnList={stnList} handler={getDest} />
-							</Grid>
-							<Grid item xs={0}>
-								<LocalizationProvider dateAdapter={AdapterDateFns}>
-									<DatePicker
-										views={["day"]}
-										open={datePickerOpen}
-										onClick={() => setDatePickerOpen(true)}
-										onClose={() => setDatePickerOpen(false)}
-										minDate={new Date()}
-										maxDate={new Date().setMonth(new Date().getMonth() + 3)}
-										label="Date of Journey"
-										value={value}
-										inputFormat="dd/MM/yyyy"
-										onChange={(newValue) => {
-											setValue(newValue);
-										}}
-										renderInput={(params) => (
-											<TextField
-												style={{ backgroundColor: "#FFFFFF" }}
-												onKeyDown={(e) => {
-													if (e.code !== "Tab" && !e.ctrlKey) {
-														e.preventDefault();
-													}
-												}}
-												variant="outlined"
-												{...params}
-												sx={{ width: "50%" }}
-												value={value}
-												onClick={() => setDatePickerOpen(true)}
-											/>
-										)}
-									/>
-								</LocalizationProvider>
-							</Grid>
-							<Grid item xs={0}>
-								<TextField
-									id="outlined-select-class-type-native"
-									select
-									variant="outlined"
-									label="Class Type"
-									style={{ backgroundColor: "#FFFFFF" }}
-									onChange={(e) => {
-										rqstParam.classType = classes[e.target.value].code;
-									}}
-									SelectProps={{
-										native: true,
-									}}
-								>
-									{classes.map((option) => (
-										<option key={option.value} value={option.value}>
-											{option.label}
-										</option>
-									))}
-								</TextField>
-							</Grid>
-							<Grid item xs={0}>
-								<Button
-									style={{
-										backgroundColor: "#DC532D",
-										color: "#FFFFFF",
-										width: "88px",
-										height: "55px",
-										fontSize: "14px",
-									}}
-									variant="contained"
-									onClick={searchTrain}
-								>
-									Search
-								</Button>
-							</Grid>
-						</Grid>
-					</div>
-				</Container>
-				<div
-					style={{ marginTop: "50px" }}
-					className="search_result"
-					fadeIn={fadeIn}
-				>
-					{result && data.map((val, index) => (<TrainResults data={val} classes={classes} />))}
-				</div>
-			</div>
+  function getStns() {
+    fetch("data/stations")
+      .then((response) => response.json())
+      .then((data) => {
+        setStnList(data);
+      });
+  }
+  useEffect(() => {
+    getStns();
+  }, []);
+  
+  return (
+    <>
+      <div>
+        <Box className="box" moveDown={moveUp} maxWidth="sm"></Box>
+      </div>
+    <div className="search_field" moveUp={moveUp}>
+        <Container
+          maxWidth="md"
+          style={{
+            marginTop: "180px",
+          }}
+        >
+          <Typography
+            style={{ color: "#242038", fontWeight: 550 }}
+            variant="h3"
+            color="common.white"
+            justifyContent="center"
+            align="center"
+            position="relative"
+            gutterBottom
+          >
+            Your Journey starts here
+          </Typography>
+        </Container>
+      <Container align="center">
+        <div>
+          <Grid container spacing={0} align="center" justifyContent="center">
+            <Grid item xs={0}>
+              <StnAutoComplete
+                label="From"
+                stnList={stnList}
+                handler={getSrc}
+              />
+            </Grid>
+            <Grid item xs={0}>
+              <StnAutoComplete label="To" stnList={stnList} handler={getDest} />
+            </Grid>
+            <Grid item xs={0}>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                  views={["day"]}
+                  open={datePickerOpen}
+                  onClick={() => setDatePickerOpen(true)}
+                  onClose={() => setDatePickerOpen(false)}
+                  minDate={new Date()}
+                  maxDate={new Date().setMonth(new Date().getMonth() + 3)}
+                  label="Date of Journey"
+                  value={value}
+                  inputFormat="dd/MM/yyyy"
+                  onChange={(newValue) => {
+                    setValue(newValue);
+                  }}
+                  renderInput={(params) => (
+                    <TextField
+                      style={{ backgroundColor: "#FFFFFF" }}
+                      onKeyDown={(e) => {
+                        if (e.code !== "Tab" && !e.ctrlKey) {
+                          e.preventDefault();
+                        }
+                      }}
+                      variant="outlined"
+                      {...params}
+                      sx={{ width: "50%" }}
+                      value={value}
+                      onClick={() => setDatePickerOpen(true)}
+                    />
+                  )}
+                />
+              </LocalizationProvider>
+            </Grid>
+            <Grid item xs={0}>
+              <TextField
+                id="outlined-select-class-type-native"
+                select
+                variant="outlined"
+                label="Class Type"
+                style={{ backgroundColor: "#FFFFFF" }}
+                onChange={(e) => {
+                  rqstParam.classType = classes[e.target.value].code;
+                }}
+                SelectProps={{
+                  native: true,
+                }}
+              >
+                {classes.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </TextField>
+            </Grid>
+            <Grid item xs={0}>
+              <TextField
+                select
+                variant="outlined"
+                label="Passengers"
+                style={{ backgroundColor: "#FFFFFF" }}
+                onChange={(e) => {
+                  rqstParam.pass = passes[e.target.value].code;
+                }}
+                SelectProps={{
+                  native: true,
+                }}
+              >
+                {passes.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </TextField>
+            </Grid>
+            <Grid item xs={0}>
+              <Button
+                style={{
+                  backgroundColor: "#DC532D",
+                  color: "#FFFFFF",
+                  width: "88px",
+                  height: "55px",
+                  fontSize: "14px",
+                }}
+                variant="contained"
+                onClick={searchTrain}
+              >
+                Search
+              </Button>
+            </Grid>
+          </Grid>
+        </div>
+      </Container>
+    <div
+          style={{ marginTop: "50px" }}
+          className="search_result"
+          fadeIn={fadeIn}
+        >
+    {result && data.map((val, index)=>(<TrainResults data={val} classes={classes}/>))}
+    </div>
+    </div>
 
 			<div>
 				<img src={trainimg} alt="trainimg" style={{width:"100%", position:"absolute"}}/>
 			</div>
-		</>
-	);
+    </>
+  );
 }
 
 export default TrainBwStation;
